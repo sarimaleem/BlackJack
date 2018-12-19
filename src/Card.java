d9bd2d3d16773f8c95347ab1c5d1c8e3ec113127
sarimaleem <sarimaleem99@gmail.com>
2018-12-19 13:23:20 -0600

    black jack

---
+++ src/Card.java
@@ -0,0 +1,19 @@
+public class Card {
+    int val;
+    String suite;
+
+    public Card(String val, String suite) {
+
+    }
+
+    public String getSuite() {
+        return suite;
+    }
+
+    public String getVal() {
+        return val;
+    }
+
+
+
+}
