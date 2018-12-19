93e5479cb41c82b9262f27882fc76963587c25c2
sarimaleem <sarimaleem99@gmail.com>
2018-12-19 13:37:40 -0600

    card

--- src/Card.java
+++ src/Card.java
@@ -1,19 +1,39 @@
 public class Card {
     int val;
     String suite;
+    String face;
+    boolean aceVal;
 
-    public Card(String val, String suite) {
-
+    public Card(String face, String suite) {
+        this.suite = suite;
+        this.face = face;
     }
 
     public String getSuite() {
         return suite;
     }
 
-    public String getVal() {
-        return val;
+    public int getVal() {
+
+        if (face.equals("A")) {
+            if (aceVal) {
+                return 11;
+            }
+            return 1;
+        }
+
+        try {
+            return Integer.parseInt(face);
+        } catch (Exception e) {
+            return 10;
+        }
     }
 
+    public String getFace() {
+        return face;
+    }
 
-
+    public void setAceVal(boolean b) {
+        aceVal = b;
+    }
 }
