6a0a7f29629d414fdaf7235174432ac311bcfebc
Sarim Aleem <sarimaleem99@gmail.com>
2018-12-19 15:41:30 -0600

    stuff

--- src/Player.java
+++ src/Player.java
@@ -4,8 +4,7 @@ public class Player {
     String name;
     ArrayList<Card> cards;
     int coins;
-
-
+    
     public Player(String name) {
         cards = new ArrayList<>();
         this.name = name;
@@ -31,31 +30,21 @@ public class Player {
             }
         }
 
-        if (aces > 0) {
-            int[][] posvals = new int[aces][aces];
-
+        int finalTotal = total + aces;
             for (int i = 0; i < aces; i++) {
                 int temp = total;
 
                 temp += i * 11;
                 temp += (aces - i);
 
-                posvals[i][aces - i] = temp;
-            }
-
-            total = posvals[0][0];
-
-            for (int i = 0; i < posvals.length; i++) {
-                for (int j = 0; j < posvals.length; j++) {
-                    if (total > 21 && posvals[i][j] < total) {
-                        total = posvals[i][j];
-                    } else if (total < 21 && posvals[i][j] > total) {
-                        total = posvals[i][j];
-                    }
+                if (finalTotal > 21) continue;
+                if (finalTotal <= 21 && temp <= 21 && temp > finalTotal) {
+                    finalTotal = temp;
                 }
             }
-        }
-        return total;
+
+
+        return finalTotal;
     }
 
 
